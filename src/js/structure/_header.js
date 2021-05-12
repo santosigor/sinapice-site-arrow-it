4cdf0ffd417a72c47f2d85207bde2609ed78d340
Igor Santos <igor.santos@vilt-group.com>
2021-05-12 19:48:35 -0300

    dev

--- src/js/structure/_header.js
+++ src/js/structure/_header.js
@@ -15,3 +15,26 @@ $(document).ready(function (){
   });
 
 });
+
+var aitCurrentUrl = window.location.href;
+
+$(window).on('load', function () {
+
+  // item active no menu
+  $('.ait-structure__header nav a').removeClass('active');
+  if (aitCurrentUrl.indexOf('/index') > -1) {
+    $('.ait-structure__header nav a:nth-child(1)').addClass('active');
+  }
+  if (aitCurrentUrl.indexOf('/sobre-arrow-it') > -1) {
+    $('.ait-structure__header nav a:nth-child(2)').addClass('active');
+  }
+  if (aitCurrentUrl.indexOf('/servicos') > -1) {
+    $('.ait-structure__header nav a:nth-child(3)').addClass('active');
+  }
+  if (aitCurrentUrl.indexOf('/projetos') > -1 || aitCurrentUrl.indexOf('/projetos-integra') > -1) {
+    $('.ait-structure__header nav a:nth-child(4)').addClass('active');
+  }
+  if (aitCurrentUrl.indexOf('/contato') > -1) {
+    $('.ait-structure__header nav a:nth-child(5)').addClass('active');
+  }
+});
