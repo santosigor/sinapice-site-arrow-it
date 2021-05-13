41fc96002a58de6215a9fdfa39cd9d2d1b7e4151
Igor Santos <igor.santos@vilt-group.com>
2021-05-13 09:25:51 -0300

    dev

--- src/js/components/_tab.js
+++ src/js/components/_tab.js
@@ -4,8 +4,8 @@ $(document).ready(function (){
     e.preventDefault();
     var svcItem = $(this).attr('ait-tab-item');
     if(!$(this).hasClass('active')){
-      $('.ait-component__tab__nav li, .ait-component__tab__content').removeClass('active');
-      $('.ait-component__tab__content').hide();
+      $(this).closest('.ait-component__tab').find('.ait-component__tab__nav li, .ait-component__tab__content').removeClass('active');
+      $(this).closest('.ait-component__tab').find('.ait-component__tab__content').hide();
       $(this).closest('.ait-component__tab').find('.ait-component__tab__content[ait-tab-content="'+svcItem+'"]').fadeIn();
       $(this).addClass('active');
     }
