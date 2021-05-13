7640f886160266019f856b280e698b2012e20603
Igor Santos <igor.santos@vilt-group.com>
2021-05-13 01:47:55 -0300

    dev

---
+++ src/js/content/_sobre-arrow.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+
+  $('.ait-content__estimulo__bg__carrosel').slick({
+	  arrows: true,
+    centerMode: true,
+    infinite: false,
+    // dots: true,
+    slidesToShow: 1,
+    vertical: true,
+    verticalSwiping: true
+	});
+
+});
