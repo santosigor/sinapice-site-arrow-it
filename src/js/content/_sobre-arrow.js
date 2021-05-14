905e3006239178ed9ee4df8d78b0b619138484d6
Igor Santos <igor.santos@vilt-group.com>
2021-05-14 20:55:59 -0300

    dev

--- src/js/content/_sobre-arrow.js
+++ src/js/content/_sobre-arrow.js
@@ -4,41 +4,45 @@ $(document).ready(function (){
 	  arrows: true,
     centerMode: true,
     infinite: false,
-    speed: 800,
+    speed: 1000,
     // dots: true,
     slidesToShow: 1,
     vertical: true,
     verticalSwiping: true
 	});
 
-	$('.ait-content__metodologia__carrossel').slick({
-	  dots: false,
-	  infinite: false,
-	  speed: 1200,
-	  slidesToShow: 6,
-	  slidesToScroll: 1,
-	  variableWidth: true,
-	  responsive: [
-	    {
-	      breakpoint: 992,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1
-	      }
-	    }
-	  ]
-	});
-
 	aitCarouselCenter('.ait-content__parceria .ait-component__carousel', 6);
 
-	// if($(window).width() < 768){
-	// 	$('.ait-content__nosso-time__cards__carrossel').slick({
-	// 	  arrows: true,
-	//     centerMode: true,
-	//     infinite: false,
-	//     // dots: true,
-	//     slidesToShow: 1
-	// 	});
-	// }
+	aitCarouselCenter('.ait-content__metodologia__carrossel .ait-component__carousel', 6, 3, 2);
+
+	if($('.ait-content__nosso-time__cards__item').length > 5 || $(window).width() < 768){
+		var elemt = '.ait-content__nosso-time__cards__carrossel';
+		$(elemt).slick({
+		  dots: true,
+		  infinite: false,
+		  speed: 800,
+		  variableWidth: true,
+		  slidesToShow: 5,
+		  slidesToScroll: 5,
+		  responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+					  slidesToShow: 5,
+					  slidesToScroll: 5
+		      }
+		    },
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    }
+		  ]
+		});
+	  $(elemt).find('.slick-next').appendTo($(elemt).find('.slick-dots'));
+	  $(elemt).find('.slick-prev').prependTo($(elemt).find('.slick-dots'));
+	}
 
 });
