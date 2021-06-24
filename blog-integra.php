e5fbc9f68e5ba4a572c2f44269d3981139d2377d
Hugo Santos <Hugo.Santos@PE06VTQT.vilt.local>
2021-06-24 01:21:54 -0300

    Integração ao PHP

--- blog-integra.php
+++ blog-integra.php
@@ -4,7 +4,7 @@
   if(isset($_GET["id"])&&$_GET["id"]!=""){
     $id_post = $_GET["id"];
 
-    $sql = "SELECT id_post, titulo, segmento, imagem, autor, foto_autor, cargo_autor, DATE_FORMAT(data_cadastro, '%Y-%m-%d') as datacad, conteudo
+    $sql = "SELECT id_post, titulo, segmento, imagem, autor, foto_autor, cargo_autor, DATE_FORMAT(data_cadastro, '%Y-%m-%d') as datacad, conteudo, id_categoria
     FROM ait_blog_post
     WHERE id_post = $id_post
     ORDER BY id_post DESC";
@@ -18,6 +18,7 @@
     $nome_autor = $row["autor"];
     $cargo_autor = $row["cargo_autor"];
     $conteudo = $row["conteudo"];
+    $id_categoria = $row["id_categoria"];
 
     $data_cadastro = $row["datacad"];
     setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
@@ -90,6 +91,59 @@
         </div>
       </div>
     </section>
+    <?
+      $sql = "SELECT id_post, titulo, segmento, imagem, DATE_FORMAT(data_cadastro, '%Y-%m-%d') as datacad, id_categoria
+      FROM ait_blog_post
+      WHERE id_categoria = $id_categoria AND id_post != $id_post
+      ORDER BY data_cadastro DESC
+      LIMIT 4";
+      $rs = mysqli_query($con, $sql); 
+      if(mysqli_num_rows($rs)>0){
+    ?>
+    <section class="ait-content__blog__relacionados">
+      <div class="ait-utilities__text-align__center">
+        <span class="ait-component__tag">Conteúdos relacionados</span>
+      </div>
+      <div class="ait-container">
+        <div class="ait-component__carousel center">
+            <?
+              while($row = mysqli_fetch_array($rs)){
+                $id_post = $row["id_post"];
+                $titulo = $row["titulo"];
+                $segmento = $row["segmento"];
+                $imagem = $row["imagem"];
+                $data_cadastro = $row["datacad"];
+
+                $d = explode("-", $data_cadastro);
+                $data_cadastro = $d[2]."/".$d[1]."/".$d[0];
+
+                $id_categoria = $row["id_categoria"];
+
+                if($id_categoria==1){
+                  $nomeclass = "text";
+                }else if($id_categoria==2){
+                  $nomeclass = "video";
+                }
+            ?>
+                <div>
+                  <div class="ait-content__blog__card">
+                    <a href="blog-integra.php?id=<?=$id_post?>">
+                      <div class="ait-content__blog__card__img" style="background-image: url('arrowit-admin/img/blog_post/<?=$imagem?>');">
+                        <span class="ait-content__blog__card__img__icon <?=$nomeclass?>"></span>
+                        <span class="ait-content__blog__card__img__categoria"><?=$segmento?></span>
+                      </div>
+                      <div class="ait-content__blog__card__info">
+                        <span><?=$data_cadastro?></span>
+                        <strong><?=$titulo?></strong>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+            <?}?>
+        </div>
+      </div>
+    </section>
+    <?}?>
 
     <footer class="ait-structure__footer"></footer>
 
