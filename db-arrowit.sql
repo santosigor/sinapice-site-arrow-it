cfce184cb93fc018fc95e990fe7b6b48688dc9b2
Igor Santos <igor.santos@vilt-group.com>
2021-09-20 21:08:00 -0300

    atualizando conteudos

--- db-arrowit.sql
+++ db-arrowit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Set-2021 às 01:48
+-- Tempo de geração: 21-Set-2021 às 02:07
 -- Versão do servidor: 10.4.20-MariaDB
 -- versão do PHP: 8.0.8
 
